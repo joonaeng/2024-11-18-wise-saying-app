50d08d2b21490e079442d628a3289442094bf6c4
joonaeng <jylee33286315@gmail.com>
2024-11-19 17:21:43 +0900

    ãpush

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,7 @@
 package com.ll;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -13,6 +13,18 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("명령) ");
+
+            String cmd = scanner.nextLine();
+            if(cmd.equals("종료")) break;
+            else if(cmd.equals("등록")) {
+                
+            }
+        }
+
+        scanner.close();
     }
 }
