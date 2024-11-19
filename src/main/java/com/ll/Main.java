e8d21980db02fbc7fe27bc8e4b219d6a17de37f5
joonaeng <jylee33286315@gmail.com>
2024-11-19 14:14:29 +0900

    push-1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,7 +4,15 @@ package com.ll;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
 
+class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
 
+        System.out.print("명령) ");
     }
 }
