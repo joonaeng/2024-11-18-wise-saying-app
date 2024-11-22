6eabb62f4dc9c007ecb9a8a6acacb263f17b47f8
joonaeng <jylee33286315@gmail.com>
2024-11-22 12:32:18 +0900

    최적화ì시도중

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -12,24 +12,26 @@ public class Main {
 }
 
 class App {
+    private final Scanner scanner;
+    private int lastId;
+    private final List<WiseSaying> ws;
+
+    public App() {
+        scanner = new Scanner(System.in); //스캐너 객체 생성
+        lastId = 0;
+        ws = new ArrayList<>(); //ArrayList 생성 WiseSaying 클래스의 배열을 생성
+    }
+
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        int lastId = 0; //마지막 기록 번호
-
-        Scanner scanner = new Scanner(System.in); //스캐너 객체 생성
-        List<WiseSaying> ws = new ArrayList<>(); //ArrayList 생성 WiseSaying 클래스의 배열을 생성
-
         while(true) {
             System.out.print("명령) ");
-
-            String cmd = scanner.nextLine(); //다음 줄을 읽어오기
-
-            String[] parts = cmd.split("\\?");
+            String cmd = scanner.nextLine(); //cmd 값 받아오기
+            String[] parts = cmd.split("\\?"); //cmd의 값을 '?'를 기준으로 분류
 
             if(parts[0].equals("삭제")) { //명언 삭제
                 String param = parts[1];
-
                 String[] KeyValue = param.split("=");
                 if(KeyValue[0].equals("id")) {
                     int Value = Integer.parseInt(KeyValue[1]);
@@ -42,7 +44,8 @@ class App {
                         }else System.out.printf("%d번 명언은 존재하지 않습니다.\n",Value);
                     }
                 }
-            }else if(parts[0].equals("수정")) { //명언 수정
+            }
+            else if(parts[0].equals("수정")) { //명언 수정
                 String param = parts[1];
 
                 String[] KeyValue = param.split("=");
@@ -62,45 +65,53 @@ class App {
                     }
                 }
             }
-            else if(cmd.equals("종료")) break;
             else if(cmd.equals("등록")) {
-
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                int id = ++lastId;
-
-                WiseSaying wiseSaying = new WiseSaying(id, content, author); //WiseSaying 클래스의 객체에 id, content, author 저장
-
-                ws.add(wiseSaying); //wiseSaying[n]에 id, content, author 주소 저장
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+                upload();
             }
             else if(cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("---------------------------");
-                if(lastId == 0) {
-                    System.out.println("등록된 명언이 없습니다.");
-                }
-                else for(int i = 0; i <lastId ; i++) {
-                    if(ws.get(i)!=null)
-                        System.out.printf("%d / %s / %s\n", ws.get(i).id, ws.get(i).author, ws.get(i).content); //저장된 주소의 값에 접근해서 값 가져오기
-                        //주소가 null 값이 아닐 때 ws(wiseSaying 배열).get(주소 번호).id(내용)
-                }
+                showList();
             }
+            else if(cmd.equals("종료")) break;
+
         }
         scanner.close();
+    }
+
+    public void upload() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        int id = ++lastId;
+        // id, content, author의 값을 받아옴
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author); //WiseSaying 클래스의 객체에 id, content, author 저장
+        ws.add(wiseSaying); //wiseSaying[n]에 id, content, author 주소 저장
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+    }
+
+    public void showList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("---------------------------");
+
+        if(lastId == 0) { //명언을 등록하지 않고 확인하려 했을 때
+            System.out.println("등록된 명언이 없습니다.");
+        }
+        else for(WiseSaying wiseSaying : ws) {//질문하기 향상된 for문 사용 전에는 int i 인자를 사용해서 ws.get(i)값으로 가져옴
+            if(wiseSaying!=null) {
+                System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.content); //저장된 주소의 값에 접근해서 값 가져오기
+            }//주소가 null 값이 아닐 때 ws(wiseSaying 배열).get(주소 번호).id(내용)
+        }
     }
 }
 
 class WiseSaying {
-    int id;
-    String content;
-    String author;
+    public int id;
+    public String content;
+    public String author;
 
-    WiseSaying(int id, String content, String author) {
+    public WiseSaying(int id, String content, String author) {
         this.id = id;
         this.content = content;
         this.author = author;
